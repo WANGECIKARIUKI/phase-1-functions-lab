b40ca6f60ba22ef2dbf7841c92afe4142d9a4117
LUCY <wangecikariu.luc@gmail.com>
2023-07-16 17:55:43 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function index.js()
+
+function distanceFromHqInBlocks(43)
+
+function distanceInFeet() {
+    distanceFromHqInBlocks(43); 
+    return
+}
+
+function distanceTravelledInFeet() {
+    returns the number of feet traveled
+}
+
+function calculateFarePrice() {
+    returns the far for the customer
+}
