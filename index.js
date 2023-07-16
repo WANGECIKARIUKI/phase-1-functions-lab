c4fd315e509bae399b1f1801c464f91ce8cc88a2
LUCY <wangecikariu.luc@gmail.com>
2023-07-16 19:14:11 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 // Code your solution in this file!
 function index.js()
 
-function distanceFromHqInBlocks(43)
-
-function distanceInFeet() {
-    distanceFromHqInBlocks(43); 
-    return
+function distanceFromHqInBlocks(distanceInBlocks) {
+    
+    if (distanceInBlocks>42) {
+        return (distanceInBlocks - 42);
+    }
+    else{
+        return (42 - distanceInBlocks);
+    }
+  
 }
 
-function distanceTravelledInFeet() {
-    returns the number of feet traveled
+function distanceFromHqInFeet(distanceInBlocks) {
+    
+    if (distanceFromHqInBlocks>42) {
+    return (distanceFromHqInBlocks - 42) * 264;
+    }
+    else{
+        return (42 - distanceFromHqInBlocks) * 264;
+
+    }
 }
 
-function calculateFarePrice() {
-    returns the far for the customer
+function distanceTravelledInFeet(start, destination) {
+
+    if (destination>start) {
+        return (start - destination) * 264;
+    }
+    else {
+        return (destination - start) * 264;
+    }
+}    
+
+function calculateFarePrice(start, destination) {
+let distance =distanceTravelledInFeet(start, destination);
+
+if (distance>=400) {
+    return (0);
+     }
+if (distance>400 && distance<=2000) {
+    return (distance - 400) * 0.02;
+}
+
+else if (distance>=2000 && distance<=2500) {
+    return (25);
+}
 }
